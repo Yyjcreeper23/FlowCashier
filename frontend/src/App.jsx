0929283f6f350192861fb254720da1a77fef3224
Alexander Yong <ayon0027@student.monash.edu>
2026-08-16 14:15:50 +0800

    Remove unnecessary useRef import

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { useCallback, useState, useEffect, useRef } from "react";
+import { useCallback, useState, useEffect } from "react";
 import Calendar from "react-calendar";
 import CurrencyInput from "./CurrencyInput";
 import "react-calendar/dist/Calendar.css";
